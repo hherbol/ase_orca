7951c1c11043a5a23f8fb2e5fdb0cf33baab5871
Henry Herbol <hherbol@gmail.com>
2016-06-29 17:52:34 -0400

    Added another example script

---
+++ examples/orca_SP/SP.py
@@ -0,0 +1,39 @@
+from ase.io import read, write
+from ase.neb import NEB
+from ase.calculators.orca import orca
+from ase.optimize import BFGS
+
+# Optimize using ASE optimization method and single point calculations
+def method_1():
+	# Read in second to last frame
+	image = read("../xyz/CNH_HCN.xyz",'-2')
+
+	# Set the calculator
+	image.set_calculator(orca(label="CNH", RunTyp="Gradient"))
+
+	# Run optimizer
+	dyn = BFGS(image, trajectory='CNH.traj')
+	dyn.run(fmax=0.1)
+
+	# Final output
+	print "Energy via Method 1 = ", image.get_potential_energy(), "eV"
+	# Resave trajectory to xyz
+	traj = read("CNH.traj",':')
+	write("CNH.xyz",traj)
+
+# Use OPT in orca
+def method_2():
+	# Read in second to last frame
+	image = read("../xyz/CNH_HCN.xyz",'-2')
+
+	# Set the calculator
+	image.set_calculator(orca(label="CNH", RunTyp="Opt"))
+
+	# Run the simulation to get the potential energy
+	# Note - it does not recalculate anything as the system itself
+	#        hasn't changed between calls to get_potential_energy()
+	image.get_potential_energy()
+	print "Energy via Method 2 = ", image.get_potential_energy(), "eV"
+
+method_1()
+method_2()
