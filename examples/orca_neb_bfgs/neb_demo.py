f0cb2c5804191ecd780a7ab0a54f34c049595573
Henry Herbol <hherbol@gmail.com>
2017-01-02 12:01:59 -0500

    Updates to example codes

--- examples/orca_neb_bfgs/neb_demo.py
+++ examples/orca_neb_bfgs/neb_demo.py
@@ -1,9 +1,13 @@
+import print_helper
+
 ALPHA = 0.1
-FMAX = 0.1
+FMAX = 0.03
+FRMS = 0.001
+MAX_STEP = 0.2  # 0.2 is better, but to keep with ASE we use the same
 RIGID_ROTATION = True
 
 
-def method_ASE():
+def method_ASE(RIGID_ROTATION, FPTR="../xyz/CNH_HCN.xyz"):
     from ase.io import read, write
     from ase.neb import NEB
     from ase.calculators.orca import orca
@@ -17,7 +21,9 @@ def method_ASE():
     chdir("ASE")
 
     start, images = [], []
-    FPTR = "../../xyz/CNH_HCN.xyz"
+    FPTR = "../" + FPTR
+    if not FPTR.endswith(".xyz"):
+        FPTR += ".xyz"
 
     for a in read(FPTR, ':'):
         start += [a]
@@ -25,7 +31,8 @@ def method_ASE():
 
     # Setup neb
     neb = NEB(images, parallel=False,
-              remove_rotation_and_translation=RIGID_ROTATION)
+              remove_rotation_and_translation=RIGID_ROTATION,
+              method="improvedtangent")
     # Set the calculator
     for image in images:
         image.set_calculator(orca(label="CNH_HCN", RunTyp="Gradient"))
@@ -58,27 +65,44 @@ def method_CLANCELOT(opt_method="LBFGS"):
 
     print("\nRUNNING CLANCELOT SIMULATION WITH RIGID_ROTATION %s...\n" % is_on)
 
-    run_name = 'CNH_HCN_clancelot'
+    run_name = 'CNH_HCN_c_' + opt_method
     new_opt_params = {'step_size': ALPHA,
                       'step_size_adjustment': 0.5,
-                      'max_step': 0.2,
+                      'max_step': MAX_STEP,
                       'linesearch': 'backtrack',
                       'accelerate': True,
                       'reset_step_size': 5,
-                      'g_rms': convert("eV/Ang", "Ha/Ang", 0.03),
+                      'g_rms': convert("eV/Ang", "Ha/Ang", FRMS),
                       'g_max': convert("eV/Ang", "Ha/Ang", FMAX)}
+
     opt = neb.NEB(run_name,
                   frames,
                   route,
                   k=convert_energy("eV", "Ha", 0.1),
                   opt=opt_method,
                   new_opt_params=new_opt_params)
-    opt.optimize()
-    write_xyz(frames, "CNH_HCN_opt_clancelot")
+    output = opt.optimize()
+    frames = output[-1]
+
+    write_xyz(frames, "CNH_HCN_opt_%s" % opt_method)
 
     print("\nDONE WITH CLANCELOT SIMULATION...\n")
 
 
-method_ASE()
+# We run ASE, then Clancelot, then we take the last frame in clancelot and
+# validate it within ASE. If it converges in 1 iteration, and Clancelot
+# converged faster than ASE originally, then Clancelot is validated as being
+# faster.
+
+s = print_helper.color_set("VERIFYING CLANCELOT AGAINST ASE", "BLUE")
+s = print_helper.color_set(s, "BOLD")
+
+method_ASE(RIGID_ROTATION)
+
 method_CLANCELOT(opt_method="BFGS")
+print(s)
+method_ASE(False, "CNH_HCN_opt_BFGS")
+
 method_CLANCELOT(opt_method="LBFGS")
+print(s)
+method_ASE(False, "CNH_HCN_opt_LBFGS")
