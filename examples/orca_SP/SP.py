20058ec0fc67599bfa81bb6a739b6687f3be2317
Henry Herbol <hherbol@gmail.com>
2017-01-02 10:42:45 -0500

    Updates to examples

--- examples/orca_SP/SP.py
+++ examples/orca_SP/SP.py
@@ -1,39 +1,43 @@
 from ase.io import read, write
-from ase.neb import NEB
 from ase.calculators.orca import orca
 from ase.optimize import BFGS
 
+
 # Optimize using ASE optimization method and single point calculations
 def method_1():
-	# Read in second to last frame
-	image = read("../xyz/CNH_HCN.xyz",'-2')
+    print("Running Method 1 - Optimization via ASE optimizer.")
+    # Read in second to last frame
+    image = read("../xyz/CNH_HCN.xyz", '-2')
 
-	# Set the calculator
-	image.set_calculator(orca(label="CNH", RunTyp="Gradient"))
+    # Set the calculator
+    image.set_calculator(orca(label="CNH", RunTyp="Gradient"))
 
-	# Run optimizer
-	dyn = BFGS(image, trajectory='CNH.traj')
-	dyn.run(fmax=0.1)
+    # Run optimizer
+    dyn = BFGS(image, trajectory='CNH.traj')
+    dyn.run(fmax=0.1)
 
-	# Final output
-	print "Energy via Method 1 = ", image.get_potential_energy(), "eV"
-	# Resave trajectory to xyz
-	traj = read("CNH.traj",':')
-	write("CNH.xyz",traj)
+    # Final output
+    print "Energy via Method 1 = ", image.get_potential_energy(), "eV"
+    # Resave trajectory to xyz
+    traj = read("CNH.traj", ':')
+    write("CNH.xyz", traj)
+
 
 # Use OPT in orca
 def method_2():
-	# Read in second to last frame
-	image = read("../xyz/CNH_HCN.xyz",'-2')
+    print("Running Method 2 - Optimization via Orca.")
+    # Read in second to last frame
+    image = read("../xyz/CNH_HCN.xyz", '-2')
 
-	# Set the calculator
-	image.set_calculator(orca(label="CNH", RunTyp="Opt"))
+    # Set the calculator
+    image.set_calculator(orca(label="CNH", RunTyp="Opt"))
 
-	# Run the simulation to get the potential energy
-	# Note - it does not recalculate anything as the system itself
-	#        hasn't changed between calls to get_potential_energy()
-	image.get_potential_energy()
-	print "Energy via Method 2 = ", image.get_potential_energy(), "eV"
+    # Run the simulation to get the potential energy
+    # Note - it does not recalculate anything as the system itself
+    #        hasn't changed between calls to get_potential_energy()
+    image.get_potential_energy()
+    print "Energy via Method 2 = ", image.get_potential_energy(), "eV"
+
 
 method_1()
 method_2()
--- examples/orca_neb_bfgs/neb_demo.py
+++ examples/orca_neb_bfgs/neb_demo.py
@@ -1,70 +1,84 @@
-from sys import argv
-
-FMAX = 0.2
+ALPHA = 0.1
+FMAX = 0.1
 RIGID_ROTATION = True
 
+
 def method_ASE():
-	from ase.io import read, write
-	from ase.neb import NEB
-	from ase.calculators.orca import orca
-	from ase.optimize import BFGS
-	from os import mkdir, chdir
-	from os.path import isdir
+    from ase.io import read, write
+    from ase.neb import NEB
+    from ase.calculators.orca import orca
+    from ase.optimize import BFGS
+    from os import mkdir, chdir
+    from os.path import isdir
 
-	# Read in frames
-	if not isdir("ASE"): mkdir("ASE")
-	chdir("ASE")
+    # Read in frames
+    if not isdir("ASE"):
+        mkdir("ASE")
+    chdir("ASE")
 
-	start, images = [], []
-	FPTR = "../../xyz/CNH_HCN.xyz"
-	#FPTR = "xyz/CNH_HCN_opt.xyz"
-	for a in read(FPTR,':'):
-	    start += [a]
-	    images += [a.copy()]
+    start, images = [], []
+    FPTR = "../../xyz/CNH_HCN.xyz"
 
-	# Setup neb
-	neb = NEB(images, parallel=False, remove_rotation_and_translation=RIGID_ROTATION)
-	# Set the calculator
-	for image in images:
-	    image.set_calculator(orca(label="CNH_HCN", RunTyp="Gradient"))
+    for a in read(FPTR, ':'):
+        start += [a]
+        images += [a.copy()]
 
-	# Run neb
-	dyn = BFGS(neb, trajectory='CNH_HCN.traj')
-	dyn.run(fmax=FMAX)
+    # Setup neb
+    neb = NEB(images, parallel=False,
+              remove_rotation_and_translation=RIGID_ROTATION)
+    # Set the calculator
+    for image in images:
+        image.set_calculator(orca(label="CNH_HCN", RunTyp="Gradient"))
 
-	# Save?
-	traj = read("CNH_HCN.traj",':-1')
-	write("../CNH_HCN_opt_ASE.xyz",traj)
+    # Run neb
+    dyn = BFGS(neb, trajectory='CNH_HCN.traj')
+    dyn.run(fmax=FMAX)
 
-	chdir("..")
-	print("\nDONE WITH ASE SIMULATION...\n")
+    # Save?
+    traj = read("CNH_HCN.traj", ':-1')
+    write("../CNH_HCN_opt_ASE.xyz", traj)
 
-def method_CLANCELOT():
-	from files import read_xyz, write_xyz
-	from neb import neb
-	from units import convert, convert_energy
+    chdir("..")
+    print("\nDONE WITH ASE SIMULATION...\n")
 
-	FPTR = "./../xyz/CNH_HCN.xyz"
-	frames = read_xyz(FPTR)
-	route = '! HF-3c'
 
-	run_name = 'CNH_HCN_clancelot'
-	neb(run_name, frames, route, k=convert_energy("eV","Ha",0.1), opt="BFGS", maxiter=1000, gtol=convert("eV/Ang","Ha/Ang",0.03), fmax=convert("eV/Ang","Ha/Ang",FMAX), DFT='orca', alpha=0.1, fit_rigid=RIGID_ROTATION, reset=100)
-	write_xyz(frames,"CNH_HCN_opt_clancelot")
+def method_CLANCELOT(opt_method="LBFGS"):
+    from files import read_xyz, write_xyz
+    import neb
+    from units import convert, convert_energy
 
-	print("\nDONE WITH CLANCELOT SIMULATION...\n")
+    FPTR = "./../xyz/CNH_HCN.xyz"
+    frames = read_xyz(FPTR)
+    route = '! HF-3c'
 
-if len(argv) > 1 and argv[1].lower() == "ase":
-	print("Running ASE NEB of CNH Isomerization.\n")
-	method_ASE()
-elif len(argv) > 1 and argv[1].lower() == "clancelot":
-	print("Running Clancelot NEB of CNH Isomerization.\n")
-	method_CLANCELOT()
-elif len(argv) > 1 and argv[1].lower() == "all":
-	print("Running both ASE and Clancelot NEB of CNH Isomerization.\n")
-	method_ASE()
-	method_CLANCELOT()
-else:
-	print("Running default: ASE NEB of CNH Isomerization.")
-	print("\tNote: Other options are (1) ase, (2) clancelot and (3) all.\n")
-	method_ASE()
+    if RIGID_ROTATION:
+        is_on = "ON"
+    else:
+        is_on = "OFF"
+
+    print("\nRUNNING CLANCELOT SIMULATION WITH RIGID_ROTATION %s...\n" % is_on)
+
+    run_name = 'CNH_HCN_clancelot'
+    new_opt_params = {'step_size': ALPHA,
+                      'step_size_adjustment': 0.5,
+                      'max_step': 0.2,
+                      'linesearch': 'backtrack',
+                      'accelerate': True,
+                      'reset_step_size': 5,
+                      'g_rms': convert("eV/Ang", "Ha/Ang", 0.03),
+                      'g_max': convert("eV/Ang", "Ha/Ang", FMAX)}
+    opt = neb.NEB(run_name,
+                  frames,
+                  route,
+                  k=convert_energy("eV", "Ha", 0.1),
+                  opt=opt_method,
+                  new_opt_params=new_opt_params)
+    opt.optimize()
+    write_xyz(frames, "CNH_HCN_opt_clancelot")
+
+    print("\nDONE WITH CLANCELOT SIMULATION...\n")
+
+
+method_ASE()
+method_CLANCELOT(opt_method="BFGS")
+method_CLANCELOT(opt_method="LBFGS")
